a6319a8fdab1986258abf7bacd1af24977ece357
Philipp Lackner <mail@pl-coding.com>
2023-03-23 12:56:12 +0100

    Implementing feature

--- app/src/main/java/com/plcoding/appsweepyoutubedemo/MainActivity.kt
+++ app/src/main/java/com/plcoding/appsweepyoutubedemo/MainActivity.kt
@@ -15,6 +15,7 @@ import com.plcoding.appsweepyoutubedemo.ui.theme.AppsweepYoutubeDemoTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        println("HELLO WORLD")
         setContent {
             AppsweepYoutubeDemoTheme {
 
